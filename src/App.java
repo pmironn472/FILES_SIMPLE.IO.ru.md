a4341c50fe70a6434306c5ae1043034b0fdf8840
pmironn472 <63416798+pmironn472@users.noreply.github.com>
2020-05-19 14:47:07 +0300

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -10,30 +10,28 @@ public class App {
         String pizza = "Pizza 75.00 2\n";
         String kebab = "Kebab 45.00 1\n";
         String cola = "Cola 9.50 3";
-        String totalprice= "The total price will be >>>>>> ";
-        double totalPrice = 75.00+45.00+9.50;
+        String totalprice = "The total price will be >>>>>> ";
+        double totalPrice = 75.00 + 45.00 + 9.50;
 
         FileWriter fw = new FileWriter("orders.txt");
-        fw.write(pizza + kebab + cola + "\n"+totalprice+ totalPrice);
+        fw.write(pizza + kebab + cola + "\n" + totalprice + totalPrice);
         fw.close();
-
 
 
         File file = new File("orders.txt");
 
 
-        if (file.exists())
+        if (file.exists()) {
             System.out.println("File Found");
-        else System.err.println("File Not Found");
+            Scanner in = new Scanner(file);
+            for (int i = 0; i < 4; i++) {
+                String data = in.nextLine();
+                System.out.println(data);
+            }
+        } else System.err.println("File Not Found");
 
 
         System.out.println();
-
-        Scanner in = new Scanner(file);
-            for(int i = 0; i < 4;i++){
-                String data = in.nextLine();
-                System.out.println(data);
-        }
 
 
     }
