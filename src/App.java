554b9a86b0cf5fd1c7ec3104404a8014776975ef
pmironn472 <63416798+pmironn472@users.noreply.github.com>
2020-05-20 22:01:35 +0300

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -1,37 +1,52 @@
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.InputMismatchException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        String pizza = "Pizza 75.00 2\n";
-        String kebab = "Kebab 45.00 1\n";
-        String cola = "Cola 9.50 3";
-        String totalprice = "The total price will be >>>>>> ";
-        double totalPrice = 75.00 + 45.00 + 9.50;
 
-        FileWriter fw = new FileWriter("orders.txt");
-        fw.write(pizza + kebab + cola + "\n" + totalprice + totalPrice);
-        fw.close();
+        writeText();
+
+        readText();
 
 
-        File file = new File("orders.txt");
 
 
-        if (file.exists()) {
-            System.out.println("File Found");
-            Scanner in = new Scanner(file);
-            for (int i = 0; i < 4; i++) {
-                String data = in.nextLine();
-                System.out.println(data);
-            }
-        } else System.err.println("File Not Found");
 
+    }
 
-        System.out.println();
+    public static void writeText()  {
+
+        BufferedReader  in = new BufferedReader(new InputStreamReader(System.in));
+        Stream<String> stream = in.lines().limit(4);
+        try (PrintWriter pw = new PrintWriter("Orders.txt", StandardCharsets.UTF_8)){
+            stream.map(s -> s).forEachOrdered(pw::println);
+
+        }catch (IOException e){
+            System.out.println("Error");
+        }
+
+    }
+
+    public static void readText() {
+        try {
+            File file = new File("Orders.txt");
+                Scanner in = new Scanner(file);
+                for (int i = 0; i < 4; i++) {
+                    String data = in.nextLine();
+                    System.out.println(data);
+                }
+        }catch (FileNotFoundException e){
+            System.out.println("Error, file not found");
+        }catch (NoSuchElementException e){
+            System.out.println("No data");
+        }finally {
+            System.out.println();
+            System.out.println("Thanks , for using our app");
+        }
 
 
     }
