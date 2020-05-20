9d6dad52defcb00aeb827df3ff3f19b34300604c
pmironn472 <63416798+pmironn472@users.noreply.github.com>
2020-05-20 22:49:03 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class App {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         writeText();
 
